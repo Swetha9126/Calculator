3c8b3abfa3125d56005c8f2eae7314df93c18e21
Swetha9126 <swethamurugesan062@gmail.com>
2025-04-02 10:07:57 +0530

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Calculator from './Calculator/Calculator'
-import JsonExample from './jsonexample'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Calculator/>
